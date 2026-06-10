61d29d5f761dd19e09a67b3b3e8835dabad05c2f
mnaglak <51410453+mnaglak@users.noreply.github.com>
2026-06-10 15:02:50 -0400

    Update NSQ.js

--- NSQ.js
+++ NSQ.js
@@ -7,9 +7,6 @@ var mapOptions = {
  center: [37.10556, 25.37], //set center Lat/Long of your area of interest
  zoom: 16, //set initial zoom level
  maxZoom : 20,  //set max zoom level
- minZoom: 10,
- touchZoom: true,
-
  }
 
 var map = new L.map('map', mapOptions);
@@ -18,3 +15,4 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
             maxNativeZoom: 17,
             maxZoom: 20,
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
+Esri_WorldImagery.addTo(map);
